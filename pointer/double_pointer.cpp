cfc13017464b638cd810508b0693513919b4ab13
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-02-14 21:09:29 +0530

    double pointer

--- pointer/double_pointer.cpp
+++ pointer/double_pointer.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 
@@ -37,44 +36,4 @@ int main(){
     cout<<a<<endl;
     cout<<b<<endl;
     return 0;
-=======
-#include<iostream>
-using namespace std;
-
-void solve(int**q){
-//    q=q+1;
-//    *q=*q+1;
-    **q=**q+1;
-}
-
-int main(){
-    // int a=5;
-    // int*p=&a;
-    // int**q=&p;
-
-    // cout<<a<<endl;
-    // cout<<&a<<endl;
-    // cout<<p<<endl;
-    // cout<<&p<<endl;
-    // cout<<*p<<endl;
-    // cout<<q<<endl;
-    // cout<<&q<<endl;
-    // cout<<*q<<endl;
-    // cout<<**q<<endl;
-
-    // int a=5;
-    // int *p=&a;
-    // int **q=&p;
-
-    // solve(q);
-    // cout<<a<<endl;
-
-    int a=5;
-    // creating reference variable
-    int &b=a;
-
-    cout<<a<<endl;
-    cout<<b<<endl;
-    return 0;
->>>>>>> 7c28d0fd0a0978ede2f8675bf2beb28af517ee45
 }

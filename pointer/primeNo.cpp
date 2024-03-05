2a4605d81af10cac9e9b2dea5362a24f329520f6
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-05 22:59:14 +0530

    write sieve eratosthemes approach

--- pointer/primeNo.cpp
+++ pointer/primeNo.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 using namespace std;
+
+//sieve eratosthemes
+int countPrime(int n){
+    if(n == 0) return 0;
+
+    vector<bool>prime(n,true);  //all are prime marked already
+    prime[0]=prime[1]=false;
+
+    int ans =0;
+    for(int i=2;i<n;i++){
+        if(prime[i]){
+            ans++;
+
+            int j=2*i;
+            while(j<n){
+                prime[j]=false;
+                j+=i;
+            }
+        }
+    }
+    return ans; 
+}
 
 //Square root approach.
 bool isPrime(int n){
@@ -30,16 +53,16 @@ bool isPrime(int n){
 //     return true;
 // }
 
-int countPrime(int n){
-    int count=0;
+// int countPrime(int n){
+//     int count=0;
 
-    for(int i=2;i<=n;i++){
-        if(isPrime(i)){
-            count++;
-        }
-    }
-    return count;
-}
+//     for(int i=2;i<=n;i++){
+//         if(isPrime(i)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
  
 

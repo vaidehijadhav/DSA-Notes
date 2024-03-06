9db74cf7ea0dd53d89f284ae446a5680bcc9dc36
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-06 11:51:41 +0530

    use euclidean approach

--- pointer/basic_math/gcd.cpp
+++ pointer/basic_math/gcd.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int a, int b){
+    if(a == 0) return b;
+
+    if(b == 0) return a;
+
+    while(a>0 && b>0){
+        if(a>b){
+            a=a-b;
+        }
+        else{
+            b=b-a;
+        }
+    }
+    return a == 0 ? b:a;
+}
+
 int main(){
-    
+    int a;
+    cout<<"Enter the value of a: "<<endl;
+    cin>>a;
+    int b;
+    cout<<"Enter the value of b: "<<endl;
+    cin>>b;
+
+    cout<<"GCD of "<<a<<" and "<<b<<" is: "<<gcd(a,b);
+
     return 0;
 }
